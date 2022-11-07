85283d66081053c53956a58872e9bb30c0e9e4ae
Amin <aminsono714@gmail.com>
2022-11-06 21:12:16 -0500

    fix
    fix? I might have understood it wrong? Reread it

--- Lab8/lab8cc3/scripts.js
+++ Lab8/lab8cc3/scripts.js
@@ -1,6 +1,7 @@
 //This will output when the page loads
-document.write("Output Zero Here");
 
+//confused if you wanted me to document.write("Output Zero Here");
+// or if you wanted me to make seprate buttons for all or if it should just display and show nothing then when button is pressed they show? Let me know confused on the wording a bit
 //This will output when a button is pressed
 function therest(){
 	document.getElementById("output1").innerHTML="1";
