00fe0abe6ce6753b63845f900dd73d989a39a428
Amin <aminsono714@gmail.com>
2022-11-06 21:10:09 -0500

    fix?

--- Lab8/lab8cc3/scripts.js
+++ Lab8/lab8cc3/scripts.js
@@ -3,6 +3,9 @@ document.write("Output Zero Here");
 
 //This will output when a button is pressed
 function therest(){
-	document.getElementById("output3").innerHTML="Output Three Here";
+	document.getElementById("output1").innerHTML="1";
+	document.getElementById("output2").innerHTML="2";
+	document.getElementById("output3").innerHTML="3";
+	document.getElementById("output0").innerHTML="0";
 
 }
