01087be345870113c25e00b54005ac883c5821da
Amin <aminsono714@gmail.com>
2022-11-14 00:18:40 -0500

    ..

--- Lab9/cc2/scripts.js
+++ Lab9/cc2/scripts.js
@@ -1,11 +1,7 @@
+
 function getAge(){
-if (myPrompt != null) {
-print.document.getElementById("theYear").innerHTML = "Your birth year is " + (2022 - myPrompt);
-}
+document.write("Your birth year is " + (2022- myPrompt));
 }
 
 var myPrompt = prompt("What is your age?");
 console.log(myPrompt);
-
-//<button type="button" onclick="getAge()">Click.</button>
-//<button type="button" onclick="getAge()">Click.</button>
