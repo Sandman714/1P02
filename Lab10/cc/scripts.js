7416469a931b4f9fd3233a2f9667394b48d6e49b
Amin <aminsono714@gmail.com>
2022-11-20 20:05:41 -0500

    lab10 only calc?

---
+++ Lab10/cc/scripts.js
@@ -0,0 +1,26 @@
+
+function getCalculationadd(){
+    num1 = document.getElementById("basicTextArea").value;
+    num2 = document.getElementById("basicTextAreaTwo").value;
+  document.getElementById("label").innerHTML = Number(num1) + Number(num2);}
+
+  function getCalculationsub(){
+      num1 = document.getElementById("basicTextArea").value;
+      num2 = document.getElementById("basicTextAreaTwo").value;
+    document.getElementById("label").innerHTML = Number(num1) - Number(num2);}
+
+    function getCalculationmult(){
+        num1 = document.getElementById("basicTextArea").value;
+        num2 = document.getElementById("basicTextAreaTwo").value;
+      document.getElementById("label").innerHTML = Number(num1) * Number(num2);}
+
+      function getCalculationdiv(){
+          num1 = document.getElementById("basicTextArea").value;
+          num2 = document.getElementById("basicTextAreaTwo").value;
+        document.getElementById("label").innerHTML = Number(num1) / Number(num2);}
+
+
+var num1 = document.getElementById("basicTextArea").value;
+var num2 = document.getElementById("basicTextAreaTwo").value;
+
+// Image was broken in Brightspace and also brightspace has a couple issues that might benifit who ever runs it to fix for a quality of life update. Such as HTML editing should not produce a popup where not only textbox is small, but it doesent update the acual URL, just the link to the URL? It is weird and you have to hit update after which makes it problamitic for people myself and other losing marks on. Folders can fixed ect. anyplace to make these sudgestions?
